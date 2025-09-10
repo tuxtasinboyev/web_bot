3bc56a7940631dd01d093af09746fb72fb759fe1
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2025-09-10 17:23:53 +0500

    buldi

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,25 @@ import Login from "./pages/Login";
 import OwnerDashboard from "./pages/OwnerDashboard";
 import UserDashboard from "./pages/userDashboard";
 import AdminDashboard from "./pages/adminDashboard";
-// import TenantDashboard from "./pages/TenantDashboard";
 
 function App() {
-  const [user, setUser] = useState(null); // { role: "OWNER" / "TENANT", ... }
-
   return (
     <Router>
       <Routes>
         <Route
-          path="/"
-          element={
-            !user ? <Navigate to="/login" /> : <Navigate to={`/${user.role.toLowerCase()}`} />
-          }
-        />
-
-        <Route
           path="/login"
-          element={
-            !user ? <Login onSuccess={(u) => setUser(u)} /> : <Navigate to={`/${user.role.toLowerCase()}`} />
-          }
+          element={<Login />}
         />
 
         <Route
           path="/register"
-          element={
-            !user ? <Register onSuccess={(r) => setUser({ role: r })} /> : <Navigate to={`/${user.role.toLowerCase()}`} />
-          }
+          element={<Register />}
         />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
 
         <Route path="/owner" element={<OwnerDashboard />} />
 
@@ -45,14 +36,14 @@ function App() {
           element={<AdminDashboard />}
         />
 
-        <Route
+        {/* <Route
           path="*"
           element={
             <div className="flex items-center justify-center h-screen">
               <h1 className="text-xl font-bold text-red-600">❌ 404 - Sahifa topilmadi</h1>
             </div>
           }
-        />
+        /> */}
       </Routes>
     </Router>
   );
