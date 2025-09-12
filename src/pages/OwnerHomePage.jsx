39f591adec46daedc1ca488aca6f3087ca9c0608
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2025-09-12 18:19:53 +0500

    bulsid

--- src/pages/OwnerHomePage.jsx
+++ src/pages/OwnerHomePage.jsx
@@ -180,7 +180,7 @@ export default function HomePage() {
 
       {/* Telefon raqami modal oynasi */}
       {isPhoneModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50 p-4">
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-300 p-4">
           <div className="bg-white rounded-lg max-w-sm w-full p-6">
             <div className="flex justify-between items-center mb-4">
               <h3 className="text-lg font-bold">Telefon raqami</h3>
